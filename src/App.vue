6e6d95cbfb9d0204cd9ee220f711187e7bd92a4e
Aung Phyo Kywe <aungphyokywe@Aungs-MacBook-Pro.local>
2020-08-22 23:59:14 +0630

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <AddTodo/>
+    <FilterTodos/>
+    <Todo/>
+  </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Todo from './components/Todos'
+import AddTodo from './components/AddTodo'
+import FilterTodos from './components/FilterTodos'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+    Todo,
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: #ee88ee;
+}
+.container{
+  max-width: 1100px;
+  margin:auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
